d4d761137c9f7975db110a371fb11ab017c8558a
cernayoel478-max <cernayoel478@gmail.com>
2026-09-01 18:57:08 -0600

    Recrear proyecto desde cero - menú y batalla funcional

--- scripts/battle_arena.gd
+++ scripts/battle_arena.gd
@@ -1,12 +1,9 @@
 extends Node2D
 
-@onready var player1_health_bar = $UI/HBoxContainer/Player1Health
-@onready var player2_health_bar = $UI/HBoxContainer/Player2Health
+func _ready():
+	print("Batalla iniciada")
 
-func _ready() -> void:
-	print("Arena de batalla lista!")
-
-func _process(_delta: float) -> void:
-	# Volver al menú con ESC
+func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
+		print("Volviendo al menú...")
 		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,15 +1,12 @@
 extends Control
 
-func _ready() -> void:
-	print("Menú principal cargado")
+func _ready():
+	print("Menú cargado")
 
-func _on_play_pressed() -> void:
-	print("Botón JUGAR presionado")
+func _on_play_pressed():
+	print("Entrando a batalla...")
 	get_tree().change_scene_to_file("res://scenes/battle_arena.tscn")
 
-func _on_settings_pressed() -> void:
-	print("Abriendo configuración...")
-
-func _on_quit_pressed() -> void:
-	print("Saliendo del juego...")
+func _on_quit_pressed():
+	print("Saliendo...")
 	get_tree().quit()
